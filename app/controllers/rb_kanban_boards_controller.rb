cbd049ed0e3f2cfe93ed8d9ea5ed09a1b4d90583
Jesal Rathod <jesalrathod@qdata.io>
2020-09-02 15:16:58 +0530

    DB Connection not working

--- app/controllers/rb_kanban_boards_controller.rb
+++ app/controllers/rb_kanban_boards_controller.rb
@@ -15,6 +15,25 @@ class RbKanbanBoardsController < RbApplicationController
   end
 
   def show
+    staging-243593-sancho-project.wsuite.com:
+    adapter: mysql2
+    database: project_243593
+    host: stage-52137-mysqldb.wsuite.com
+    username: project_243593
+    password: BvbYwS70orzxVUTLW43P9uqM2
+    default_db: staging-243593-sancho-project.wsuite.com
+    encoding: utf8
+    variables:
+        sql_mode:
+        "no_auto_value_on_zero,\
+      strict_trans_tables,\
+      no_zero_date,\
+      strict_all_tables,\
+      no_zero_in_date,\
+      error_for_division_by_zero,\
+      no_auto_create_user,\
+      no_engine_substitution"
+
     @sprint = KanbanBoard.find params[:sprint_id]
     if @sprint.wi_id.present?
       @selectedworkflow = @sprint.wi_id
@@ -30,12 +49,20 @@ class RbKanbanBoardsController < RbApplicationController
       p base_url = OpenProject::Configuration.project_base_url.gsub(/https:\/\/|http:\/\//, "")
       p default_brand_db = ActiveRecord::Base.configurations[base_url]["default_db"]
       p Rails.env
-      ActiveRecord::Base.connect_to(default_brand_db) do
+      ActiveRecord::Base.establish_connection(
+          adapter: "mysql2",
+          database: "project_243593",
+          host: "stage-52137-mysqldb.wsuite.com",
+          username: "project_243593",
+          password: "BvbYwS70orzxVUTLW43P9uqM2",
+          default_db: "staging-243593-sancho-project.wsuite.com"
+      )
+      #ActiveRecord::Base.connect_to(default_brand_db) do
         p "++++++++++ Connection Successfull with Default DB +++++++++++++"
         p wi = WorkflowInformation.first
         p @wi_id = WorkflowInformation.find_by(name: @selected_workflow_information).id
         p @workfows_status = WorkflowStatus.where(wi_id: @wi_id)
-      end
+      #end
     else
       p "--------Inside Else Condition---------"
       @workfows_status = WorkflowStatus.where(wi_id: @selectedworkflow)
