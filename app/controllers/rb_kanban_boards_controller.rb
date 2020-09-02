5a00a57812c628854983672275239cdd44b7345c
Jesal Rathod <jesalrathod@qdata.io>
2020-09-02 18:42:18 +0530

    DB Connection not working

--- app/controllers/rb_kanban_boards_controller.rb
+++ app/controllers/rb_kanban_boards_controller.rb
@@ -30,24 +30,28 @@ class RbKanbanBoardsController < RbApplicationController
       p base_url = OpenProject::Configuration.project_base_url.gsub(/https:\/\/|http:\/\//, "")
       p default_brand_db = ActiveRecord::Base.configurations[base_url]["default_db"]
       p Rails.env
-      # ActiveRecord::Base.establish_connection(
-      #     adapter: "mysql2",
-      #     database: "project_243593",
-      #     host: "stage-52137-mysqldb.wsuite.com",
-      #     username: "project_243593",
-      #     password: "BvbYwS70orzxVUTLW43P9uqM2",
-      #     default_db: "staging-243593-sancho-project.wsuite.com"
-      # )
-      ActiveRecord::Base.connect_to(:staging_243593_sancho_project) do
+      db = ActiveRecord::Base.establish_connection(
+          adapter: "mysql2",
+          database: "project_243593",
+          host: "stage-52137-mysqldb.wsuite.com",
+          username: "project_243593",
+          password: "BvbYwS70orzxVUTLW43P9uqM2",
+          default_db: "staging-243593-sancho-project.wsuite.com"
+      )
+      #ActiveRecord::Base.connect_to(:staging_243593_sancho_project) do
         p "++++++++++ Connection Successfull with Default DB +++++++++++++"
         p wi = WorkflowInformation.first
         p @wi_id = WorkflowInformation.find_by(name: @selected_workflow_information).id
         p @workfows_status = WorkflowStatus.where(wi_id: @wi_id)
-      end
+        ActiveRecord::Base.remove_connection(db)
+      #end
     else
       p "--------Inside Else Condition---------"
       @workfows_status = WorkflowStatus.where(wi_id: @selectedworkflow)
     end
+    p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+    p WorkflowInformation.first
+    p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
     p "************** WORKFLOW STATUS **************************"
     p @workfows_status
     p "************** WORKFLOW STATUS **************************"
